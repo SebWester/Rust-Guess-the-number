a019c0903dc78004ff9db2c4ae22fc3147ebd4ef
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-02-08 22:43:22 +0100

    Game loop added

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,30 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
-
-    // Type String
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    // Konvertera guess från String till Int --> u32
-    // trim() --> Ta bort "vita" tecken = Mellanslag/Radbrytning
-    // parse() --> Omvandlar strängen till datatyp som anges
-    // expect() --> Felmeddelande som körs om parse() returnerar ett fel
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!!"),
-        Ordering::Equal => println!("That's right!"),
+    loop {
+        println!("Please input your guess.");
+    
+        // Type String
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        // Convert guess from String to Int --> u32
+        // trim() --> Remove "white" chars = Spaces/Line breaks
+        // parse() --> Converting String to specified type
+        // expect() --> Error if parse() returns error
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Equal => {
+                println!("That's right!"); 
+                break;
+            }                
+        }
     }
 }
